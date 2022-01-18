be0621ff2e683f67e7b23c6ac89284a6f427d9f3
Mikhail <melnikov_mn@mail.ru>
2022-01-18 20:24:14 +0300

    добаавлены локаторы и новые проверки

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.browser.current_url, "URL is not corrected"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form doesn't present"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form doesn't present"
